b8616e32401adf883bec6328ec9aec8f0e958bd6
Ani Daniel <danielani660@gmail.com>
2021-12-29 07:37:48 +0100

    recent upate

--- routes/user.js
+++ routes/user.js
@@ -69,4 +69,31 @@ router.put('/:id',upload.single('image'),async(req,res)=>{
     }
 })
 
+// b64 image upload
+router.post(`/img`, function (req, res) {
+ 
+    var dataURI = req.body.dataURI;
+    var type=req.body.type;
+    // Should check the image type
+    var uploadStr = `data:image/${type};base64,` + dataURI;
+
+    cloudinary.v2.uploader.upload(uploadStr, {
+        overwrite: true,
+        invalidate: true,
+        width: 810, height: 456, crop: "fill"
+    },
+        function (error, result) {
+            res.json(result);
+        });
+});
+/**
+ * router.paost('/uploadBae64', (req,res)=>{
+ * let filtPath=`/files/${Date.now()}_${req.body.name}})`;
+ * let buffer =Buffer.from (req.body.base64.split(',)[1],"base64");
+ * // Buffer contains the image 
+ * fs.writeFileSync(path.join(_dirname,filepath),buffer);
+ * 
+ * res.json(filepath);
+ */
+
 module.exports=router
